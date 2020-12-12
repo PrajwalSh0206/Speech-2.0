2da861e4855cd15232b80760f1bb59cbba649a8c
Munna0206 <prajwalshetty57@gmail.com>
2020-12-12 21:49:51 +0530

    file delete

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,23 @@
 var express = require('express');
 var router = express.Router();
 const gTTS = require('gtts');
+const fs = require('fs');
+const path = require('path');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+  fs.readdir('./public/audio',(err,files)=>{
+    for (const file of files) {
+      fs.unlink(path.join('./public/audio', file), err => {
+        if (err) throw err;
+      });
+    }
+  })
   res.render('index', { title: 'Text/Speech' });
+});
+
+router.get('/favicon.ico', function(req, res) { 
+  res.sendStatus(204); 
 });
 
 router.post('/hear', function (req, res) {
